818c77fd4de722b1540cf590287129c2002ff76a
aryanmore33 <aryanmore.1359@gmail.com>
2026-07-30 13:29:55 +0530

    match model fixed

--- server/src/businessLogic/managers/matchManager.js
+++ server/src/businessLogic/managers/matchManager.js
@@ -3,6 +3,7 @@ const MatchModel = require("../../models/matchModel");
 class MatchManager {
 
   static async getMatches(userId) {
+    console.log("Current User:", userId);
     const model = new MatchModel();
     return model.findMatches(userId);
   }
--- server/src/models/matchModel.js
+++ server/src/models/matchModel.js
@@ -2,10 +2,8 @@ const Db = require("../models/libs/Db");
 const db = Db.getQueryBuilder();
 
 class MatchModel {
-
   async findMatches(userId) {
-
-    return db
+    const query = db
       .select(
         "u.id",
         "u.name",
@@ -23,7 +21,38 @@ class MatchModel {
       .leftJoin("reviews as r", "r.reviewee_id", "u.id")
       .where("uw.user_id", userId)
       .whereNot("u.id", userId)
-      .groupBy("u.id", "u.name", "u.profile_pic", "s.name", "uos.level", "uos.id");
+
+      // Exclude users with pending or accepted barter requests
+      .whereNotExists(function () {
+        this.select(1)
+          .from("barter_requests as br")
+          .whereIn("br.status", ["pending", "accepted"])
+          .andWhere(function () {
+            this.where(function () {
+              this.where("br.requester_id", userId)
+                .whereColumn("br.receiver_id", "u.id");
+            }).orWhere(function () {
+              this.where("br.receiver_id", userId)
+                .whereColumn("br.requester_id", "u.id");
+            });
+          });
+      })
+
+      .groupBy(
+        "u.id",
+        "u.name",
+        "u.profile_pic",
+        "s.name",
+        "uos.level",
+        "uos.id"
+      );
+
+    // Debug
+    console.log("Current User:", userId);
+    console.log("SQL:", query.toSQL().sql);
+    console.log("Bindings:", query.toSQL().bindings);
+
+    return query;
   }
 }
 
